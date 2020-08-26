d4c2ac0948dcdcc0fc64cb73e3236803c03bd27f
nvakiet <63222542+nvakiet@users.noreply.github.com>
2020-08-26 09:53:04 +0700

    Add in game hotkey action according to the gamescript, lane textures

--- 19CLC5_Group10_OOP_Project/src/Main.cpp
+++ 19CLC5_Group10_OOP_Project/src/Main.cpp
@@ -6,6 +6,15 @@
 #include "trafficmanager.h"
 #include "AnimalLaneManager.h"
 #include "menu.h"
+
+
+struct gameStates {
+    bool isPaused = false;
+    bool isInGame = false;
+    bool isOver = false;
+};
+
+
 int main()
 {
     //Initialize window
@@ -16,11 +25,11 @@ int main()
     //Setup resource managers
     texture textureManager;
 
-    //Init game components
-    Player player(*textureManager.player[0]);
-    trafficmanager traffics(textureManager);
-    AnimalManager animals(textureManager);
+    //Init game status
     mainMenu mainM(textureManager);
+    gameStates status;
+    float level = 1;
+    const float maxLevel = 5;
     //Start game clock
     sf::Clock gameClock;
     sf::Clock frameClock;
@@ -30,7 +39,14 @@ int main()
     while (window.isOpen()) {
         choice = mainM.Switch(window);
         if (choice == 0) {
-            while (!player.isDead())
+            //Init game components
+            status.isInGame = true;
+            Player player(*textureManager.player[0]);
+            trafficmanager traffics(textureManager);
+            AnimalManager animals(textureManager);
+            sf::Sprite background;
+            background.setTexture(*textureManager.background);
+            while (status.isInGame && !status.isOver)
             {
                 float gameTime = gameClock.getElapsedTime().asSeconds();
                 //Event catching phase
@@ -39,6 +55,7 @@ int main()
                     switch (event.type) {
                         //These are for catching UI activity, not for game activity
                     case sf::Event::Closed:
+                        status.isInGame = false;
                         window.close();
                         break;
                     case sf::Event::KeyPressed:
@@ -54,26 +71,32 @@ int main()
                                 window.setFramerateLimit(60);
                             }
                         }
+                        if (event.key.code == sf::Keyboard::P) status.isPaused = !status.isPaused;
+                        if (event.key.code == sf::Keyboard::Escape) status.isInGame = false;
                         break;
                     }
 
                 }
-                player.move(event, lastElapse);
-                traffics.update(lastElapse, gameTime);
-                animals.update(lastElapse, gameTime);
-                //Logic phase
-                if (player.isImpact(&traffics) || player.isImpact(&animals))
-                    cout << "Vehicle or Animal hit you!" << endl;
-                if (player.isDead()) {
-                    cout << "You died!" << endl;
-                    return 0;
-                }
-                if (player.reachedGoal()) {
-                    cout << "You win!" << endl;
-                    return 1;
+                if (!status.isPaused) {
+                    player.move(event, lastElapse);
+                    traffics.update(lastElapse, gameTime);
+                    animals.update(lastElapse, gameTime);
+                    //Logic phase
+                    if (player.isImpact(&traffics) || player.isImpact(&animals))
+                        cout << "Vehicle or Animal hit you!" << endl;
+                    if (player.isDead()) {
+                        cout << "You died!" << endl;
+                        status.isOver = true;
+                    }
+                    if (player.reachedGoal()) {
+                        cout << "To next level!" << endl;
+                        if (level < maxLevel) level += 0.5;
+                        else status.isOver = true;
+                    }
                 }
                 //Drawing phase
                 window.clear();
+                window.draw(background);
                 player.draw(window);
                 traffics.draw(window);
                 animals.draw(window);
@@ -87,6 +110,8 @@ int main()
                 if (gameTime < 0.1)
                     lastElapse *= 60;
             }
+            //Reset status after jump back to main menu
+            status = gameStates();
         }
         window.clear();
         mainM.draw(window);
